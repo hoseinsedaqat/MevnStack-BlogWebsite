3ba4f24e1acb600770bc1d3cdcfbc10fbfa82bac
hsedaqat1378 <71232870+hsedaqat1378@users.noreply.github.com>
2021-07-27 02:59:35 +0430

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import App from './App.vue';
-import router from './router'
+import router from './router';
 import vuetify from './plugins/vuetify'
 import './css/style.css'
 
